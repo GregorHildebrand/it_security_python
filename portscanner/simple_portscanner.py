bd52dac674cea26566751926d817b0c4876cc522
Gregor Hildebrand <44243728+GregorHildebrand@users.noreply.github.com>
2020-11-07 21:43:02 +0100

    Update simple_portscanner.py

--- portscanner/simple_portscanner.py
+++ portscanner/simple_portscanner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright (c) 2020 Gregor Hildebrand
+# developed in 2020 by: Gregor Hildebrand
 # It is a beginner python program what is scanning one port on a specific ip-addr
 
 import socket
